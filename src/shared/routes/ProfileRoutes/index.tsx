ab6b43a04e4c2c07b389d59350017dae7801737d
gustavo_20082 <gustavo_20082@LAB.ETEC187>
2022-10-18 14:36:46 -0300

    feat: registSelf

--- src/shared/routes/ProfileRoutes/index.tsx
+++ src/shared/routes/ProfileRoutes/index.tsx
@@ -8,12 +8,16 @@ import { ForgotPass } from 'src/screens/User/ForgotPass';
 import { ResetPass } from 'src/screens/User/ResetPass';
 import { VerifyUserEmail } from 'src/screens/User/VerifyUserEmail';
 import { AuthProvider } from '@shared/hooks/contexts/AuthContext';
+import { RegistSelf } from 'src/screens/Disappeared/RegistSelf';
+import { ChangeDisappearData } from 'src/screens/Disappeared/ChangeDisappearData';
 
 export interface ProfileParamsList {
   ProfileIndex: undefined
   SignIn: unknown
   SignUp: undefined
+  RegistSelf: undefined
   ForgotPass: undefined
+  ChangeDisappearData: undefined
   ResetPass: undefined
   VerifyUserEmail: undefined
   List: undefined
@@ -24,7 +28,9 @@ const ProfileNav = createNativeStackNavigator();
 
 export const ProfileRouter: React.FC = () => (
   <AuthProvider>
-    <ProfileNav.Navigator initialRouteName="ProfileIndex" screenOptions={{ headerShown: false, contentStyle: { backgroundColor: '#fff' } }}>
+    <ProfileNav.Navigator initialRouteName="RegisteSelf" screenOptions={{ headerShown: false, contentStyle: { backgroundColor: '#fff' } }}>
+      <ProfileNav.Screen name="RegistSelf" component={RegistSelf} />
+      <ProfileNav.Screen name="ChangeDisappearData" component={ChangeDisappearData} />
       <ProfileNav.Screen name="ProfileIndex" component={Profile} />
       <ProfileNav.Screen name="SignIn" component={SignIn} />
       <ProfileNav.Screen name="SignUp" component={SignUp} />
