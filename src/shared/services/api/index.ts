d3440f10b09f7861201d40760543440cbd3fef01
Izaias Henrique <izaias.rique@gmail.com>
2022-10-23 21:25:25 -0300

    fix: merge conflict with API URL

--- src/shared/services/api/index.ts
+++ src/shared/services/api/index.ts
@@ -6,9 +6,5 @@ export interface ResponseError {
 }
 
 export const api = axios.create({
-<<<<<<< HEAD
   baseURL: 'http://192.168.0.118:3333/',
-=======
-  baseURL: 'http://192.168.15.58:3333/',
->>>>>>> ae1e232f69cb2ab582714a66c2492feec1eddb2f
 });
