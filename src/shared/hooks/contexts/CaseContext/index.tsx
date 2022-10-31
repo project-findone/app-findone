6e70e709a6d0ecbb5e83d3a8317d1e5d4af69c57
Izaias Henrique <izaias.rique@gmail.com>
2022-10-30 22:08:45 -0300

    feat: add list cases function in Case Context

--- src/shared/hooks/contexts/CaseContext/index.tsx
+++ src/shared/hooks/contexts/CaseContext/index.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/jsx-no-constructed-context-values */
-import type { PropsWithChildren } from 'react';
 import React, {
-  createContext, useContext, useCallback,
+  createContext, useContext, useCallback, PropsWithChildren, useState,
 } from 'react';
 import { AxiosError } from 'axios';
 
@@ -22,15 +21,46 @@ type TregisterCredentials = {
   description: string;
 };
 
+export type ICaseData = {
+  disappeared: {
+    name: string;
+    lastname: string;
+    personID: number;
+    personTypeID: number;
+    age: number;
+  }
+  case: {
+    city: string;
+  }
+};
+
 type ICaseContextData = {
   services: {
     register: (credentials: TregisterCredentials) => Promise<void>;
+    listCases: () => Promise<void>
   };
+  casesOfDisappeareds: ICaseData[] | null
 };
 
 const CaseContext = createContext<ICaseContextData>({} as ICaseContextData);
 
 export const UserProvider: React.FC<PropsWithChildren> = ({ children }) => {
+  const [cases, setCases] = useState<ICaseData[] | null>(null);
+
+  const listCases = useCallback(async () => {
+    try {
+      const disappeareds = await api.get('disappeareds');
+      if (disappeareds.data) setCases(disappeareds.data);
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        if (error.response) {
+          const { message } = error.response.data as ResponseError;
+          showToast({ message, type: 'alert' });
+        }
+      }
+    }
+  }, []);
+
   const register = useCallback(async (credentials: TregisterCredentials) => {
     try {
       const response = await api.post('disappeared', credentials);
@@ -48,7 +78,8 @@ export const UserProvider: React.FC<PropsWithChildren> = ({ children }) => {
 
   return (
     <CaseContext.Provider value={{
-      services: { register },
+      services: { register, listCases },
+      casesOfDisappeareds: cases,
     }}
     >
       {children}
@@ -60,7 +91,7 @@ export function useUser(): ICaseContextData {
   const context = useContext(CaseContext);
 
   if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
+    throw new Error('useAuth must be used within an CaseProvider');
   }
 
   return context;
